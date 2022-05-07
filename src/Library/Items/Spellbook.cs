fa1fc2fef15c1f9ba0fd55be39c31e29062cd5ae
Gonzalo <dasilvagonzalo21@gmail.com>
2022-05-07 20:24:26 -0300

    SpellsToBook

---
+++ src/Library/Items/Spellbook.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+
+namespace RoleplayGame
+{
+    public class SpellsBook : IElement
+    {
+        private List<Spell> spells = new List<Spell>();
+
+        public List<Spell> Spells
+        {
+            get
+            {
+                return this.spells;
+            }
+        }
+
+        public int AttackValue()
+        {
+            int value = 0;
+            foreach (Spell spell in this.Spells)
+            {
+                value += spell.AttackValue();
+            }
+            return value;
+        }
+
+
+        public int DefenseValue()
+        {
+            int value = 0;
+            foreach (Spell spell in this.Spells)
+            {
+                value += spell.DefenseValue();
+            }
+            return value;
+        }
+
+
+        public void addSpellToBook(Spell spell)
+        {
+            spells.Add(spell);
+        }
+    }
+}
+
