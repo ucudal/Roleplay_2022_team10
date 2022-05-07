509a9f8797f9e921bee97c0f0f306e24f78e00ee
IonasJospo <ionas.josponis@correo.ucu.edu.uy>
2022-05-07 01:21:39 -0300

    jkhiu

--- src/Library/Interfaces/GenericContainer.cs
+++ src/Library/Interfaces/GenericContainer.cs
@@ -1,29 +1,30 @@
-using System.Collections.Generic;
+// using System.Collections.Generic;
 
-namespace Roleplay
-{
-    public class GenericContainer<T>
-    {
-        private IList<T> elements = new List<T>();
+// namespace Roleplay
+// {
+//     public class GenericContainer<T>
+//     {
+//         private IList<T> elements = new List<T>();
 
-        public void Add(T element)
-        {
-            this.elements.Add(element);
-        }
+//         public void Add(T element)
+//         {
+//             this.elements.Add(element);
+//         }
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            return this.elements.GetEnumerator();
-        }
+//         public IEnumerator<T> GetEnumerator()
+//         {
+//             return this.elements.GetEnumerator();
+//         }
 
-        public int myElementsDamage()
-        {
-            int totalDamage;
-            foreach (T element in elements)
-            {
-                return element
+//         public int myElementsDamage()
+//         {
+//             int totalDamage;
+//             foreach (T element in elements)
+//             {
+//                 return element
             
-            }
-        }
-    }
-}
+//             }
+//         }
+//     }
+// }
+//no borren queria probar una cosa 
